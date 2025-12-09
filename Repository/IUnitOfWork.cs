301532b1673c2e3c4d2348ffc31858319cda74dc
Rashed Mamdouh <rashed.mahmoud02@eng-st.cu.edu.eg>
2025-12-09 08:30:11 +0200

    add the IRepo properties to the UOW interface

--- Repository/IUnitOfWork.cs
+++ Repository/IUnitOfWork.cs
@@ -1,7 +1,17 @@
-﻿namespace Homecare.Repository
+﻿using Homecare.Model;
+
+namespace Homecare.Repository
 {
     public interface IUnitOfWork:IDisposable
     {
-        public  Task<int> SaveDbAsync();
+        public IRepository<Patient> Patients { get;  }
+        public IRepository<Physician> Physicians { get;  }
+        public IRepository<Appointment> Appointments { get; }
+        public IRepository<Medication> Medications { get;  }
+        public IRepository<Report> Reports { get;  }
+        public IRepository<Specialization> Specializations { get;}
+
+
+        public Task<int> SaveDbAsync();
     }
 }
