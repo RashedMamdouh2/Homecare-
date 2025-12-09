46753e1e9e0547306f58dad2af1722c36cc36239
Rashed Mamdouh <rashed.mahmoud02@eng-st.cu.edu.eg>
2025-12-09 08:08:12 +0200

    create objects of models Repo in UOW

--- Repository/UnitOfWork.cs
+++ Repository/UnitOfWork.cs
@@ -6,11 +6,24 @@ namespace Homecare.Repository
     public class UnitOfWork:IUnitOfWork
     {
         private readonly ApplicationDbContext context;
-        
+        public IRepository<Patient>       Patients     { get; set; }
+        public IRepository<Physician>     Physicians     { get; set; }
+        public IRepository<Appointment>  Appointments   { get; set; }
+        public IRepository<Medication>    Medications    { get; set; }
+        public IRepository<Report>        Reports         { get; set; }
+        public IRepository<Specialization>Specializations { get; set; }
+
 
         public UnitOfWork(ApplicationDbContext context)
         {
             this.context = context;
+            Patients       = new Repository<Patient>(context);
+           Physicians      = new Repository<Physician>(context);
+           Appointments    = new Repository<Appointment>(context);
+           Medications     = new Repository<Medication   >(context);
+           Reports         = new Repository<Report >(context);
+           Specializations = new Repository<Specialization>(context);
+           
         }
         public async Task<int> SaveDbAsync()
         {
