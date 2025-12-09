931afbfb9f3dac2beea7a7aeeb2e1961dcd683ce
Rashed Mamdouh <rashed.mahmoud02@eng-st.cu.edu.eg>
2025-12-09 14:22:44 +0200

    add image service to convert to/from byte array into base64 string

---
+++ Services/ImageServices.cs
@@ -0,0 +1,31 @@
+﻿using Homecare.Model;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Microsoft.Win32.SafeHandles;
+
+namespace Homecare.Services
+{
+    public interface IImageServices
+    {
+        public  Task<byte[]> ConvertToArray(IFormFile Image);
+        public string ConvertArrayToImage(byte[] img);
+    }
+    public class ImageServices:IImageServices
+    {
+        public async Task<byte[]> ConvertToArray(IFormFile Image)
+        {
+            byte[] result ;
+            using (var stream = new MemoryStream())
+            {
+                await Image.CopyToAsync(stream);
+                result = stream.ToArray();
+                
+            }
+            return result;
+        }
+       public string ConvertArrayToImage(byte[] img)
+        {
+            string base64Image = Convert.ToBase64String(img);
+            return base64Image;
+        }
+    }
+}
