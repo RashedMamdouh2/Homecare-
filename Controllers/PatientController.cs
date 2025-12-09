d27c8d180acf02cd3be5cb21f8d07a31fbbd48af
Rashed Mamdouh <rashed.mahmoud02@eng-st.cu.edu.eg>
2025-12-09 14:19:55 +0200

    create the patient controller

--- Controllers/PatientController.cs
+++ Controllers/PatientController.cs
@@ -1,6 +1,7 @@
 ﻿using Homecare.DTO;
 using Homecare.Model;
 using Homecare.Repository;
+using Homecare.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,26 +12,27 @@ namespace Homecare.Controllers
     public class PatientController : ControllerBase
     {
         private readonly IUnitOfWork unitOfWork;
+        private readonly IImageServices imageServices;
 
-        public PatientController(IUnitOfWork unitOfWork)
+        public PatientController(IUnitOfWork unitOfWork, IImageServices imageServices)
         {
             this.unitOfWork = unitOfWork;
-            
+            this.imageServices = imageServices;
         }
         [HttpGet("GetPatient/{id:int}")]
         public async Task<IActionResult> GetPatient(int id)
         {
             var patientDB = await unitOfWork.Patients.GetById(id);
-            PatientDto patient = new PatientDto {
-                Name=patientDB.Name,
-                Phone=patientDB.Phone,
-                Address=patientDB.Address,
-                City=patientDB.City,
-                Gender=patientDB.Gender,
-                Image=patientDB.Image,
-            
+            var patient = new PatientSendDto {
+                Name = patientDB.Name,
+                Phone = patientDB.Phone,
+                Address = patientDB.Address,
+                City = patientDB.City,
+                Gender = patientDB.Gender,
+                Image = imageServices.ConvertArrayToImage(patientDB.Image),
+
             };
-            if (patient == null) { 
+            if (patient == null) {
                 return NotFound("Wrong ID");
             }
             return Ok(patient);
@@ -38,21 +40,27 @@ namespace Homecare.Controllers
         [HttpGet("GetAllPatients")]
         public IActionResult GetAllPatients()
         {
-            var patients = unitOfWork.Patients.GetAll().OrderBy(p=>p.Name).Select(p=>new PatientDto {
-                Name =  p.Name,
+
+            var patients = unitOfWork.Patients.GetAll().OrderBy(p => p.Name).Select(p => new PatientSendDto {
+                Name = p.Name,
                 Phone = p.Phone,
                 Address = p.Address,
                 City = p.City,
                 Gender = p.Gender,
-                Image = p.Image,
+                Image = imageServices.ConvertArrayToImage(p.Image),
 
 
             });
             return Ok(patients);
         }
         [HttpPost("AddPatient")]
-        public async Task<IActionResult> AddPatient(PatientDto patientDto)
+        public async Task<IActionResult> AddPatient([FromForm] PatientCreateDto patientDto)
         {
+            var allowedTypes = new[] { "image/jpeg", "image/png", "image/jpg" };
+            if (patientDto.Image != null && (!allowedTypes.Contains(patientDto.Image.ContentType.ToLower()) || patientDto.Image.Length > 1000 * 1024))
+            {
+                return BadRequest("Image Should be png, jpg or jpeg of Maximum 1000 KB Size");
+            }
             var p = new Patient
             {
                 Name = patientDto.Name,
@@ -60,12 +68,35 @@ namespace Homecare.Controllers
                 Address = patientDto.Address,
                 City = patientDto.City,
                 Gender = patientDto.Gender,
-                Image = patientDto.Image
+                Image = await imageServices.ConvertToArray(patientDto.Image)
 
             };
             await unitOfWork.Patients.AddAsync(p);
             await unitOfWork.SaveDbAsync();
-            return CreatedAtAction(nameof(GetPatient),patientDto);
+            return CreatedAtAction(nameof(GetPatient), routeValues: new { id = p.Id }, patientDto);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> RemovePatient(int id)
+        {
+            var patient = await unitOfWork.Patients.GetById(id);
+            if (patient is null) return NotFound("Wrong ID");
+            unitOfWork.Patients.Delete(patient);
+            return Ok();
+        }
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdatePatient(PatientCreateDto updated,int id)
+        {
+            var old = await unitOfWork.Patients.GetById(id);
+            if (old is null) return NotFound("Wrong ID");
+            old.Name= updated.Name;
+            old.Phone= updated.Phone;
+            old.Address= updated.Address;
+            old.City= updated.City;
+            old.Gender= updated.Gender;
+            old.Image=await imageServices.ConvertToArray(updated.Image);
+            unitOfWork.Patients.UpdateById(old);
+            return CreatedAtAction(nameof(GetPatient), routeValues: new { id = old.Id }, updated);
+
         }
 
     }
