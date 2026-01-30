2666e22875938d09e86c038326529881f1e1911b
Rashed Mamdouh <rashed.mahmoud02@eng-st.cu.edu.eg>
2026-01-30 21:16:24 +0200

    create custom validation attribute to validate the booking of appointment to prevent old datetime booking

---
+++ Backend/Validation/FutureDate.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Homecare.Validation
+{
+    public class FutureDateAttribute:ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var appointmentDate = (DateOnly)value;
+            if (appointmentDate < DateOnly.FromDateTime(DateTime.Today))
+            {
+                return new ValidationResult("Invalid Date For Appointment");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
